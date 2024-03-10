14fc10a62de169774c2df0d10fbaac02b5efba4f
yasoobzaidi <162789134+yasoobzaidi@users.noreply.github.com>
2024-03-10 15:18:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ document.addEventListener('DOMContentLoaded', function() {
         popup.style.display = 'none';
     }, 5000); // 5000 milliseconds = 5 seconds
 });
+function isMobileView() {
+    return window.innerWidth <= 1000; // You can adjust the threshold as needed
+}
+
+// Function to redirect to another HTML file if in mobile view
+function redirectToMobilePage() {
+    if (isMobileView()) {
+        window.location.href = '/MobileFilo/index.html'; // Change 'mobile_page.html' to the desired HTML file
+    }
+}
+
+// Check on initial page load
+redirectToMobilePage();
+
+// Check on window resize
+window.onresize = function() {
+    redirectToMobilePage();
+};
